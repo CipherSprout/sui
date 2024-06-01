c95e71111b20d18ac90234f1de91a7c4a04c80b5
tx-tomcat <dungnd8594@gmail.com>
2024-06-01 23:05:48 +0700

    [move][move-linter] implemen multiple overflow rules

--- external-crates/move/crates/move-compiler/src/linters/shift_overflow.rs
+++ external-crates/move/crates/move-compiler/src/linters/shift_overflow.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Move Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 //! Detect potential overflow scenarios where the number of bits being shifted exceeds the bit width of
 //! the variable being shifted, which could lead to unintended behavior or loss of data. If such a
 //! potential overflow is detected, a warning is generated to alert the developer.
@@ -8,24 +11,23 @@ use crate::{
         WarningFilters,
     },
     expansion::ast::Value_,
-    naming::ast::{BuiltinTypeName_, TypeName_, Type_},
+    naming::ast::{BuiltinTypeName_, Type_},
     parser::ast::BinOp_,
-    shared::{program_info::TypingProgramInfo, CompilationEnv},
+    shared::CompilationEnv,
     typing::{
         ast::{self as T, UnannotatedExp_},
         visitor::{TypingVisitorConstructor, TypingVisitorContext},
     },
 };
 use move_ir_types::location::Loc;
-use std::str::FromStr;
 
-use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
+use super::{LinterDiagnosticCategory, LINT_WARNING_PREFIX, SHILF_OVERFLOW_DIAG_CODE};
 
 const SHIFT_OPERATION_OVERFLOW_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
     Severity::Warning,
-    LinterDiagCategory::ShiftOperationOverflow as u8,
-    LINTER_DEFAULT_DIAG_CODE,
+    LinterDiagnosticCategory::Suspicious as u8,
+    SHILF_OVERFLOW_DIAG_CODE,
     "Potential overflow detected. The number of bits being shifted exceeds the bit width of the variable being shifted.",
 );
 
@@ -38,11 +40,7 @@ pub struct Context<'a> {
 impl TypingVisitorConstructor for ShiftOperationOverflow {
     type Context<'a> = Context<'a>;
 
-    fn context<'a>(
-        env: &'a mut CompilationEnv,
-        _program_info: &'a TypingProgramInfo,
-        _program: &T::Program_,
-    ) -> Self::Context<'a> {
+    fn context<'a>(env: &'a mut CompilationEnv, _program: &T::Program) -> Self::Context<'a> {
         Context { env }
     }
 }
@@ -50,19 +48,17 @@ impl TypingVisitorConstructor for ShiftOperationOverflow {
 impl TypingVisitorContext for Context<'_> {
     fn visit_exp_custom(&mut self, exp: &mut T::Exp) -> bool {
         // Check if the expression is a binary operation and if it is a shift operation.
-        if let UnannotatedExp_::BinopExp(lhs, op, _, rhs) = &exp.exp.value {
-            // can't do  let UnannotatedExp_::BinopExp(lhs, BinOp_::Shl | BinOp_::Shr, _, rhs) = &exp.exp.value else { return };
-            // because the op is Spanned<BinOp_> and not BinOp_
-            if matches!(op.value, BinOp_::Shl | BinOp_::Shr) {
-                match (
-                    get_bit_width(&lhs.ty.value),
-                    get_shift_amount(&rhs.exp.value),
-                ) {
-                    (Some(bit_width), Some(shift_amount)) if shift_amount >= bit_width => {
-                        report_overflow(self.env, shift_amount, bit_width, op.loc);
-                    }
-                    _ => (),
+        if let UnannotatedExp_::BinopExp(lhs, sp!(_, BinOp_::Shl | BinOp_::Shr), _, rhs) =
+            &exp.exp.value
+        {
+            match (
+                get_bit_width(&lhs.ty.value),
+                get_shift_amount(&rhs.exp.value),
+            ) {
+                (Some(bit_width), Some(shift_amount)) if shift_amount >= bit_width => {
+                    report_overflow(self.env, shift_amount, bit_width, exp.exp.loc);
                 }
+                _ => (),
             }
         }
         false
@@ -89,11 +85,8 @@ fn get_bit_width(ty: &Type_) -> Option<u128> {
 }
 
 fn get_shift_amount(value: &UnannotatedExp_) -> Option<u128> {
-    if let UnannotatedExp_::Value(v) = value {
-        match &v.value {
-            Value_::U8(v) => Some(*v as u128),
-            _ => None,
-        }
+    if let UnannotatedExp_::Value(sp!(_, Value_::U8(v))) = value {
+        Some(*v as u128)
     } else {
         None
     }
